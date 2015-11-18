0dae89a4ccd535dd06ee1fbd064d7ed5de291dff
oriol.mora <uritmb@gmail.com>
2015-11-18 11:24:51 +0100

    pruebas imagenes

---
+++ app/src/main/java/applicate/com/sigma/applicate/FragmentHomeAlumno.java
@@ -0,0 +1,28 @@
+package applicate.com.sigma.applicate;
+
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class FragmentHomeAlumno extends Fragment {
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_fragment_home_alumno, container, false);
+    }
+
+
+
+}
