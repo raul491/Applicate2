370b6c80996ff30a331458c1bb0ad439b9e5bb05
marc.ramirez <maaarrk96@gmail.com>
2015-11-19 11:18:22 +0100

    Prueba 1.0

--- app/src/main/java/applicate/com/sigma/applicate/ActivityCrearNotificacion.java
+++ app/src/main/java/applicate/com/sigma/applicate/ActivityCrearNotificacion.java
@@ -4,34 +4,25 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
-public class ActivityCrearNotificacion extends AppCompatActivity {
+public class ActivityCrearNotificacion extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_activity_crear_notificacion);
+
+        LinearLayout botonEnviar = (LinearLayout) findViewById(R.id.sendButton);
+        botonEnviar.setOnClickListener(this);
+
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_activity_crear_notificacion, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+    public void onClick(View v) {
+        Toast.makeText(this, "Notificación enviada correctamente",Toast.LENGTH_LONG).show();
+        finish();
     }
 }
