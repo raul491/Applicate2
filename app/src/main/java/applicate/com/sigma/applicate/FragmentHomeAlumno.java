5ffdb48175afdebfbd170be2ad5f07141d80c6a7
oriol.mora <uritmb@gmail.com>
2015-11-19 12:25:10 +0100

    fragment_home_alumno

--- app/src/main/java/applicate/com/sigma/applicate/FragmentHomeAlumno.java
+++ app/src/main/java/applicate/com/sigma/applicate/FragmentHomeAlumno.java
@@ -1,13 +1,15 @@
 package applicate.com.sigma.applicate;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 
-public class FragmentHomeAlumno extends Fragment {
+public class FragmentHomeAlumno extends Fragment implements View.OnClickListener {
 
 
     @Override
@@ -20,7 +22,25 @@ public class FragmentHomeAlumno extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+        View rootView = inflater.inflate(R.layout.fragment_fragment_home_alumno, container, false);
+        LinearLayout botonNotificaciones = (LinearLayout) rootView.findViewById(R.id.botonNotificaciones);
+        /*LinearLayout botonBuscar = (LinearLayout) rootView.findViewById(R.id.botonBuscar);
+        LinearLayout botonEstadisticas = (LinearLayout) rootView.findViewById(R.id.botonEstadisticas);*/
+        botonNotificaciones.setOnClickListener(this);
         return inflater.inflate(R.layout.fragment_fragment_home_alumno, container, false);
     }
 
+    @Override
+    public void onClick(View v) {
+        Intent intent = null;
+        switch(v.getId()){
+            case R.id.botonNotificaciones: intent = new Intent(getContext(),ActivityCrearNotificacion.class);
+                break;
+            /*case R.id.botonBuscar: intent = new Intent(getContext(), BuscarActivity.class);
+                break;
+            case R.id.botonEstadisticas: intent = new Intent(getContext(), ActivityGrafico.class);
+                break;*/
+        }
+        startActivity(intent);
+    }
 }
